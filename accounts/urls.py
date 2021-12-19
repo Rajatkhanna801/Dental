fb76ca183d5246362c396b3f8a2b13b4ffe3d34e
rajat <rajatkhanna801@gmail.com>
2021-12-19 18:33:17 +0530

    new_commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Registration
+from .views import Registration, NewPatientForm
 
 
 urlpatterns = [
     path("new-user/", Registration.as_view(), name="new-user"),
+    path("new-patient/", NewPatientForm.as_view(), name="new-form"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,17 @@ from django.views import View
 
 
 class Registration(View):
-    template_name = "patient_form.html"
+    template_name = "form.html"
     def get(self, request):
         context = {}
         return render(request, self.template_name, context)
 
     def post(self, request):
         return redirect('new-user')
+
+
+class NewPatientForm(View):
+    template_name = "new_form.html"
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
