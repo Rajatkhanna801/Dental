015316e0b0c2ea193f7d29f7f6ff5786d2ea5294
Rajat Khanna <85541346+Rajatkhanna801@users.noreply.github.com>
2023-09-20 14:22:43 +0530

    Update settings.py

--- Dental/settings.py
+++ Dental/settings.py
@@ -148,8 +148,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "contactdentalhybrid@gmail.com"
-EMAIL_HOST_PASSWORD = "Rajatkhanna1@"
+
 
 
 
